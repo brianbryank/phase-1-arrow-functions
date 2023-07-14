cb2d3bf4c14471436d20b9567fffd32848e8bce9
brianbryank <brianbryan0125@gmail.com>
2023-07-14 18:47:17 +0300

    completed not grade assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+//this  is not graded,,
+//just for fun and understanding
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  divide(2000 / 100);
+  console.log( divide(2000 / 100));
+
+  const square = x => x * x;
+  let x = 2;
+  let answer = (square(x));
+  console.log(answer);
+
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  let a = 3;
+  let b = 4;
+  
+  let result = add(a, b);
+  console.log(result); 
